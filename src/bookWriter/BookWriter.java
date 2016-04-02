8d93bfe993f08e633d7ce4713ba3401de4f28e8e
edeyanova <deyanovae@aol.com>
2016-04-02 16:22:44 +0300

    Update BookWriter.java

--- src/bookWriter/BookWriter.java
+++ src/bookWriter/BookWriter.java
@@ -9,23 +9,6 @@ import book.Book;
 import keywords.Keyword;
 
 public class BookWriter {
-//	public static void WriteBookToFile(Book book, String path) throws IOException{
-//		File file = new File(path);
-//		
-//		if (!file.exists()) {
-//			file.createNewFile();
-//		}
-//		
-//		try(FileWriter fw = new FileWriter(file);
-//				BufferedWriter bw = new BufferedWriter(fw)){
-//			for (Keyword keyword : book.getKeywords()) {
-//				bw.write(keyword.getStem());
-//				bw.write(" ");
-//			}
-//			bw.write(book.getGenre());
-//			bw.newLine();
-//		}
-//	}
 	
 	public static void writeBooksToFile(String path, Book... books){
 		File file = new File(path);
